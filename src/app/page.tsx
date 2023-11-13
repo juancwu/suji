7b23279e26ecfbdbf026bf27eafb625c803e7194
jc <46619361+juancwu@users.noreply.github.com>
2023-11-13 11:59:11 -0500

    remove side layout from root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import SideLayout from "./_components/client/side-layout";
-
 export default function Dashboard() {
-  return <SideLayout>some content</SideLayout>;
+  return <div>some content</div>;
 }
