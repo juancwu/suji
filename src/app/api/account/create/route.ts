b4f9b3ec61e41124139b05e54780c3031211bdc0
jc <46619361+juancwu@users.noreply.github.com>
2023-11-18 19:26:20 -0500

    add todo to validate/authorize api

--- src/app/api/account/create/route.ts
+++ src/app/api/account/create/route.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 import { maxAccountNameLen } from "@/server/db/schema";
 
+// TODO: validate/authorize request first
+
 export async function POST(req: Request) {
   const form = await req.formData();
 
